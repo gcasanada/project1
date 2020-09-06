ce525e302589c32e45ed4bd9aaf094500a54dad9
Gerald <gcasanada@gmail.com>
2020-09-06 00:48:20 -0700

    test_2

--- learning1.py
+++ learning1.py
@@ -1,5 +1,18 @@
 # Below are random stuff:D
 
-message = 'Hello, my name is Gerald'
+message_1 = """Hello, my name is Gerald.
+I am trying to learn Python."""
 
-print(message)
+print(message_1)
+
+message_2 = "This is a new message with a space from the first message."
+
+print("\n") #This means new space
+print(message_2)
+
+greeting = "Hello"
+name = "Gerald"
+
+message_3 = greeting + ", " + name + "."
+print("\n")
+print(message_3)
